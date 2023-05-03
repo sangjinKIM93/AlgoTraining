bb5fa952899a27eedc162a1b97593fea17b9b518
김상진, Kim Sang Jin <62230118+sangjinKIM93@users.noreply.github.com>
2023-05-03 22:59:16 +0900

    Create 카펫.swift

---
+++ 프로그래머스/카펫.swift
@@ -0,0 +1,20 @@
+func solution(_ brown:Int, _ yellow:Int) -> [Int] {
+    var result: [Int] = []
+    
+    if yellow == 1 {
+        return [3,3]
+    } 
+    let mid = Int(yellow/2)    
+    
+    for height in 1...mid where yellow % height == 0 {
+        let width = Int(yellow/height)
+        let totalArea = (width + 2)*(height + 2) 
+        let newBrown = totalArea - yellow
+            
+        if brown == newBrown {
+            return [width + 2, height + 2]
+        }
+    }
+    
+    return result
+}
